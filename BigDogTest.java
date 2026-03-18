6ff8dd9a354977fa96a4f4c71867d86908cea2b3
Mateo Ramos <mateoramosg07@gmail.com>
2026-03-18 20:53:02 +0100

    Delete BigDogTest.java

--- BigDogTest.java
+++
@@ -1,8 +0,0 @@
-package ejercicio6_5;// Una clase puede implementar MÚLTIPLES interfaces a la vez.
-// Aquí ResizableCircle extiende Circle (herencia) e implementa Resizable (interfaz).
-
-// Interfaz Resizable
-interface Resizable {
-    void resize(int percent); // resize modifica el tamaño según un porcentaje
-}
-
