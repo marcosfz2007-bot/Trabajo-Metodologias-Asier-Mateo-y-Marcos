aa2490f754594fc36af4648190a392d6da99f007
Mateo Ramos <mateoramosg07@gmail.com>
2026-03-18 20:54:24 +0100

    Delete CircleGOTest.java

--- CircleGOTest.java
+++
@@ -1,9 +0,0 @@
-package ejercicio6_3;
-
-// Interfaz Movable: cualquier clase que la implemente puede "moverse"
-public interface Movable {
-    void moveUp();
-    void moveDown();
-    void moveLeft();
-    void moveRight();
-}
